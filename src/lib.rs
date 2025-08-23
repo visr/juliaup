a148971fd2a873986644165b65e7885243374a27
MilesCranmer <miles.cranmer@gmail.com>
2025-08-23 23:45:11 +0100

    feat: add juliapkg binary

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,7 @@ pub mod command_update_version_db;
 pub mod config_file;
 pub mod global_paths;
 pub mod jsonstructs_versionsdb;
+pub mod julia_launcher;
 pub mod operations;
 pub mod utils;
 pub mod version_selection;
--- tests/command_completions_test.rs
+++ tests/command_completions_test.rs
@@ -1,4 +1,4 @@
-use assert_cmd::cargo::cargo_bin_cmd;
+use assert_cmd::Command;
 use predicates::prelude::*;
 
 fn test_shell_completion(shell: &str, expected_patterns: &[&str]) {
@@ -7,7 +7,8 @@ fn test_shell_completion(shell: &str, expected_patterns: &[&str]) {
         .tempdir()
         .unwrap();
 
-    let mut cmd = cargo_bin_cmd!("juliaup")
+    let mut cmd = Command::cargo_bin("juliaup")
+        .unwrap()
         .arg("completions")
         .arg(shell)
         .env("JULIA_DEPOT_PATH", depot_dir.path())
